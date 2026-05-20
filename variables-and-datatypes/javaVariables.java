152ef4306ba68844f9e93337c6c9a0788b34eda6
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-20 11:28:01 +0530

    auto update: 2026-05-20 11:28:01

---
+++ variables-and-datatypes/javaVariables.java
@@ -0,0 +1,10 @@
+public class javaVariables {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        System.out.println(a);
+        System.out.println(b);
+        String name = "Vedant";
+        System.out.println("")
+    }
+}
