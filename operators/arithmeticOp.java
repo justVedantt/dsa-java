c4a86b34fd8b20f7b4e5b9bcc6c886b4b02f91b9
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-20 20:05:30 +0530

    auto update: 2026-05-20 20:05:30

---
+++ operators/arithmeticOp.java
@@ -0,0 +1,12 @@
+
+public class arithmeticOp {
+    public static void main(String[] args) {
+     int a = 10;
+     int b = 20;
+     System.out.println("add" + (a+b));   
+     System.out.println("sub" + (a-b));   
+     System.out.println("mul" + (a*b));   
+     System.out.println("div" + (a/b));   
+     System.out.println("mod" + ());   
+    }
+}
