f201653b2ab6d63d9b03e9527a79f27acba9fd9b
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-20 22:43:16 +0530

    auto update: 2026-05-20 22:43:16

---
+++ operators/unaryOp.java
@@ -0,0 +1,9 @@
+package operators;
+
+public class unaryOp {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = ++a; // pre-increment operation
+        System.out.println(b);
+    }
+}
