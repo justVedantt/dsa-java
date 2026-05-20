b191d60e4a29f3453b0c2e86e20bae30e85cd174
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-20 13:16:23 +0530

    auto update: 2026-05-20 13:16:23

--- variables-and-datatypes/takeInput.java
+++ variables-and-datatypes/takeInput.java
@@ -1,15 +1,19 @@
 import java.util.*;
-
 public class takeInput {
     public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+        // // String input = sc.next();
+        // // System.out.println(input);
+
+        // // String name = sc.nextLine();
+        // // System.out.println("name:" + name);
+
+        // int num = sc.nextInt();
+        // System.out.println(num);
         Scanner sc = new Scanner(System.in);
-        // String input = sc.next();
-        // System.out.println(input);
-
-        // String name = sc.nextLine();
-        // System.out.println("name:" + name);
-
-        int num = sc.nextInt();
-        System.out.println(num);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println(sum);
     }
 }
