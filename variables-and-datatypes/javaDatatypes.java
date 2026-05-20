ff523cc5d844a6bcca11f0da8d143934840ca745
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-20 11:46:23 +0530

    auto update: 2026-05-20 11:46:23

--- variables-and-datatypes/javaDatatypes.java
+++ variables-and-datatypes/javaDatatypes.java
@@ -1,5 +1,21 @@
 public class javaDatatypes {
     public static void main(String[] args) {
-        
+        byte b = 23;
+        char ch = 'a';
+        short s = 87;
+        int i = 123;
+        long l = 123456789L;
+        float f = 3.14f;
+        double d = 3.1445678902345678;
+        boolean bool = true;
+        System.out.println(b);
+        System.out.println(ch);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(bool);
+
     }
 }
