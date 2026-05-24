df1ceaeb682435af2d58ff6ee204172316ab1b7a
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 18:28:18 +0530

    auto update: 2026-05-24 18:28:18

---
+++ conditional_statement/ques5.java
@@ -0,0 +1,9 @@
+package conditional_statement;
+
+public class ques5 {
+    public static void main(String[] args){
+        int marks = 18;
+        String grade = (marks >= 33) ? "Pass" : "Fail";
+        System.out.println(grade);
+    }
+}
