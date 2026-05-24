9ccdf012746d3b58d2e87fb6c89ff7cd8bfaf7e5
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 18:43:58 +0530

    auto update: 2026-05-24 18:43:58

--- conditional_statement_questions/ques.java
+++ conditional_statement_questions/ques.java
@@ -2,12 +2,54 @@ package conditional_statement_questions;
 
 public class ques {
     public static void main(String[] args){
-        /*
-        Question1:WriteaJavaprogramtogetanumberfromtheuserandprintwhetheritispositive or negative.
-        Question2:FinishthefollowingcodesothatitprintsYouhaveafeverifyourtemperatureis above 100 and otherwise prints You don't have a fever.publicclassSolution{publicstaticvoidmain(String[]args) {doubletemp=103.5;}}
-        Question3:WriteaJavaprogramtoinputweeknumber(1-7)andprintdayofweeknameusing switch case.
-        Question 4 :What will be the value of x & y in thefollowing program:publicclassSolution{publicstaticvoidmain(Stringargs[]) {inta=63,b=36;booleanx= (a<b)?true:false;inty= (a>b)?a:b;}}
-        Question5:WriteaJavaprogramthattakesayearfromtheuserandprintwhetherthatyear is a leap year or not.
+/*
+1. **Question 1:**
+   Write a Java program to get a number from the user and print whether it is positive or negative.
+
+2. **Question 2:**
+   Finish the following code so that it prints `"You have a fever"` if your temperature is above 100, and otherwise prints `"You don't have a fever"`.
+
+```java id="v2q7cp"
+public class Solution {
+    public static void main(String[] args) {
+        double temp = 103.5;
+    }
+}
+```
+
+3. **Question 3:**
+   Write a Java program to input a week number (1–7) and print the day of the week name using `switch` case.
+
+4. **Question 4:**
+   What will be the value of `x` and `y` in the following program?
+
+```java id="n3g8zk"
+public class Solution {
+    public static void main(String args[]) {
+
+        int a = 63, b = 36;
+
+        boolean x = (a < b) ? true : false;
+        int y = (a > b) ? a : b;
+    }
+}
+```
+
+5. **Question 5:**
+   Write a Java program that takes a year from the user and prints whether that year is a leap year or not.
+
          */
+
+
+// Ques 1
+   Scanner sc = new Scanner(System.in);
+   int num = sc.nextInt();
+   if(num > 0){
+    System.out.println("Positive");
+   }else {
+    System.out.println("Negative");
+   }
+
+   
     }
 }
