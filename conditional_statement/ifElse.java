1a52da86abd231eb704a25cf191d5a9bfb6233e4
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 16:59:39 +0530

    auto update: 2026-05-24 16:59:39

--- conditional_statement/ifElse.java
+++ conditional_statement/ifElse.java
@@ -2,6 +2,11 @@ package conditional_statement;
 
 public class ifElse {
     public static void main(String[] args) {
-        
+     int age = 20;
+     if(age >= 18){
+        System.out.println("Eligible");
+     } else {
+        System.out.println("Not Eligible");
+     }
     }
 }
