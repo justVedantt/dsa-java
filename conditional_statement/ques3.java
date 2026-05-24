a8b2c306adce60a5a064fa1aa96aab5dab90f7fa
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 17:48:43 +0530

    auto update: 2026-05-24 17:48:43

--- conditional_statement/ques3.java
+++ conditional_statement/ques3.java
@@ -5,6 +5,11 @@ public class ques3 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int income = sc.nextInt();
-        if(income )
+        int tax = 0;
+        if(income < 500000){
+            tax = 0;
+        } else if(income >= 500000 && income < 1000000){
+            tax = 
+        }
     }
 }
