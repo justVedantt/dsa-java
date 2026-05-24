c45797653c824af8661842e9c2bead579bc21e52
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 18:41:44 +0530

    auto update: 2026-05-24 18:41:44

---
+++ conditional_statement_questions/ques.java
@@ -0,0 +1,13 @@
+package conditional_statement_questions;
+
+public class ques {
+    public static void main(String[] args){
+        /*
+        Question1:WriteaJavaprogramtogetanumberfromtheuserandprintwhetheritispositive or negative.
+        Question2:FinishthefollowingcodesothatitprintsYouhaveafeverifyourtemperatureis above 100 and otherwise prints You don't have a fever.publicclassSolution{publicstaticvoidmain(String[]args) {doubletemp=103.5;}}
+        Question3:WriteaJavaprogramtoinputweeknumber(1-7)andprintdayofweeknameusing switch case.
+        Question 4 :What will be the value of x & y in thefollowing program:publicclassSolution{publicstaticvoidmain(Stringargs[]) {inta=63,b=36;booleanx= (a<b)?true:false;inty= (a>b)?a:b;}}
+        Question5:WriteaJavaprogramthattakesayearfromtheuserandprintwhetherthatyear is a leap year or not.
+         */
+    }
+}
