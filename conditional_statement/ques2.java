0a4943535b74f8b3c7371ec74171d34f646a64ba
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 17:30:32 +0530

    auto update: 2026-05-24 17:30:32

---
+++ conditional_statement/ques2.java
@@ -0,0 +1,12 @@
+package conditional_statement;
+
+public class ques2 {
+    public static void main(String[] args) {
+    int num = 5;
+    if(num % 2 == 0){
+        System.out.println("Even");
+    } else {
+        System.out.println("Odd");
+    }
+}   
+}
