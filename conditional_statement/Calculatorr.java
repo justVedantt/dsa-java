968c36804ca521114b6dd596d7f10bc28b2f02d7
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 18:37:09 +0530

    auto update: 2026-05-24 18:37:09

--- conditional_statement/Calculatorr.java
+++ conditional_statement/Calculatorr.java
@@ -1,7 +1,15 @@
 package conditional_statement;
+import java.util.*;
 
 public class Calculatorr {
     public static void main(String[] args){
-        int a = 10;
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        char op = sc.next().charAt(0);
+        switch(op) {
+            case '+':
+                
+        }
     }
 }
