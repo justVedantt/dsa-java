d8804a425406cba1c4f18e14b7b852d30d4d348a
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 18:32:53 +0530

    auto update: 2026-05-24 18:32:53

--- conditional_statement/Switch.java
+++ conditional_statement/Switch.java
@@ -3,5 +3,21 @@ package conditional_statement;
 public class Switch {
     public static void main(String[] args){
         int day = 4;
+        switch(day){
+            case 1:
+                System.out.println("Mon");
+                break;
+            case 2:
+                System.out.println("Tue");
+                break;
+            case 3:
+                System.out.println("Wed");
+                break;
+            case 4:
+                System.out.println("Thu");
+                break;
+            case 5:
+                System.out.println
+        }
     }
 }
