bec447ebf2eedf6ab4cde58e1e1ca8533a80a3e1
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 18:39:15 +0530

    auto update: 2026-05-24 18:39:15

--- conditional_statement/Calculatorr.java
+++ conditional_statement/Calculatorr.java
@@ -8,8 +8,15 @@ public class Calculatorr {
         int b = sc.nextInt();
         char op = sc.next().charAt(0);
         switch(op) {
-            case '+':
-                
+            case '+': System.out.println(a+b);
+            break;
+            case '-': System.out.println(a-b);
+            break;
+            case '*': System.out.println(a*b);
+            break;
+            case '/': System.out.println(a/b);
+            break;
+            default: System.out.println("Invalid Operator");
         }
     }
 }
