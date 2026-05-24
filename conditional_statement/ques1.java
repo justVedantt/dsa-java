eb81cc7ae1c51b34354984d45ca101793a5171e9
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-24 17:10:01 +0530

    auto update: 2026-05-24 17:10:01

---
+++ conditional_statement/ques1.java
@@ -0,0 +1,13 @@
+package conditional_statement;
+
+public class ques1 {
+    public static void main(String[] args){
+        int a = 10;
+        int b = 20;
+        if(a > b){
+            System.out.println(a);
+        } else {
+            System.out.println
+        }
+    }
+}
