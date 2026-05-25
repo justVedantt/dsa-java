e9fe69a135e1e6dfb4719f284bafda2ceac17b1f
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-25 22:16:58 +0530

    auto update: 2026-05-25 22:16:58

--- loops/ques4.java
+++ loops/ques4.java
@@ -1,5 +1,5 @@
 package loops;
-
+// Print a 4*4 square pattern of *
 public class ques4 {
     public static void main(String[] args){
         for(int i= 0; i < 4; i++){
---
+++ loops/ques5.java
@@ -0,0 +1,7 @@
+package loops;
+// Print reverse of a number
+public class ques5 {
+    public static void main(String[] args){
+
+    }
+}
