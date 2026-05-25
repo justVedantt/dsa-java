37989f0aa4e727fbf9661907069dcf3430d18601
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-25 21:58:06 +0530

    auto update: 2026-05-25 21:58:06

---
+++ loops/ques3.java
@@ -0,0 +1,16 @@
+package loops;
+import java.util.*;
+
+public class ques3 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        int sum = 0;
+        while(i <= n){
+            sum += i;
+            i++;
+        }
+        System.out.println(sum);
+    }
+}
