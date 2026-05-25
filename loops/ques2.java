57294f4bd50fcdcac98020340bec2b62eec1cb64
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-25 21:55:17 +0530

    auto update: 2026-05-25 21:55:17

--- loops/ques2.java
+++ loops/ques2.java
@@ -1,7 +1,14 @@
 package loops;
+import java.util.*;
 
 public class ques2 {
     public static void main(String[] args){
-        Scanner sc = 
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+        while(i <= n){
+            System.out.println(i);
+            i++;
+        }
     }
 }
