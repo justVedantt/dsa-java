313fe862cb9dd0e9231f0ee5d0a2a9b95edf59b1
Vedant Pawar <vedantpofficial@gmail.com>
2026-05-25 22:23:53 +0530

    auto update: 2026-05-25 22:23:53

--- loops/ques5.java
+++ loops/ques5.java
@@ -2,6 +2,11 @@ package loops;
 // Print reverse of a number
 public class ques5 {
     public static void main(String[] args){
-
+        int n = 12345;
+        while(n > 0){
+            int rem = n % 10;
+            System.out.print(rem);
+            n = n / 10;
+        }
     }
 }
