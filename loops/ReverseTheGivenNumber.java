f400121747d776449da65fe8e0353404e72f16ee
Vedant Pawar <vedantpofficial@gmail.com>
2026-06-18 21:04:30 +0530

    auto update: 2026-06-18 21:04:30

---
+++ loops/ReverseTheGivenNumber.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class ReverseTheGivenNumber {
+    int n = 10899;
+    int rev = 0;
+    while(n > 0){
+        int lastDigit = n % 10;
+        rev = (rev * 10) + lastDigit;
+        n = n / 10;
+    }
+}
