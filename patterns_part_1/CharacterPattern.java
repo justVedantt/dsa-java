15dafdc3e239f47375b7dab1069ce3527a163555
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-06 22:45:25 +0530

    auto update: 2026-07-06 22:45:25

--- patterns_part_1/CharacterPattern.java
+++ patterns_part_1/CharacterPattern.java
@@ -4,8 +4,10 @@ public class CharacterPattern {
     public static void main(String[] args){
         int n = 5;
         for(int i = 0; i < n; i++){
+            char ch = 'A';
             for(int j = 0; j <=i; j++){
-                System.out.print();
+                System.out.print(ch);
+                ch++;
             }
             System.out.println();
         }
