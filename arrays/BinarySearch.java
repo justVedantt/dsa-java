055c3a2e069a4796a1ade479061af6549ef51abe
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-07 00:21:11 +0530

    auto update: 2026-07-07 00:21:11

--- arrays/BinarySearch.java
+++ arrays/BinarySearch.java
@@ -9,6 +9,19 @@ public class BinarySearch {
             if(arr[mid] == key){
                 return mid;
             }
+            if(arr[mid] < key){
+                start = mid + 1;
+            } else {
+                end = mid + 1;
+            }
         }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int arr[] = {2,4,6,8,10,12,14,16};
+        int key = 10;
+
+        System.out.println(binarySearch(arr, key));
     }
 }
