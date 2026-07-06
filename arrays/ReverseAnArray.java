9d5dadd0e06cd17256b586c5a3f50cd3ce173760
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-07 00:34:24 +0530

    auto update: 2026-07-07 00:34:24

--- arrays/ReverseAnArray.java
+++ arrays/ReverseAnArray.java
@@ -22,8 +22,8 @@ public class ReverseAnArray {
     public static void main(String[] args){
         int numbers[] = {2,4,6,8,10};
         reverse(numbers);
-        for(int i = 0; i < arr.length; i++){
-            
+        for(int i = 0; i < numbers.length; i++){
+            System.out.print(numbers[i] + ",");
         }
     }
 }
