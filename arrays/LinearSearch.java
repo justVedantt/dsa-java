be24d93bf1d1a17d31d7fc7abd9f3da2d2cae00e
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-06 23:59:16 +0530

    auto update: 2026-07-06 23:59:16

---
+++ arrays/LinearSearch.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class LinearSearch {
+
+    public static void linearSearch(int arr[], int key){
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i] == key){
+                
+            }
+        }
+    }
+    public static void main(String[] args){
+        int arr[] ={2,4,6,8,10,12,14,16};
+        int key = 10;
+
+        
+
+    }
+}
