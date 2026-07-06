d8ce649f41ae3d276b720d0d0b981e61366aa06e
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-06 21:36:00 +0530

    auto update: 2026-07-06 21:36:00

--- patterns_part_1/InvertedStarPattern.java
+++ patterns_part_1/InvertedStarPattern.java
@@ -2,9 +2,9 @@ package patterns_part_1;
 
 public class InvertedStarPattern {
     public static void main(String[] args){
-        int max = 4;
-        for(int i = 0; i < max; i++){
-            for(int j = 0; j < max; j++){
+        
+        for(int i = 0; i < 4; i++){
+            for(int j = 3; j >= 0; j--){
                 System.out.print("*");
             }
             System.out.println();
