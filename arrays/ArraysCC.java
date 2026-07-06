e9ad3a4222cbd9a9db5d8105f52331fd4dcd82f2
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-06 23:36:08 +0530

    auto update: 2026-07-06 23:36:08

--- arrays/ArraysCC.java
+++ arrays/ArraysCC.java
@@ -1,8 +1,15 @@
+
+
 package arrays;
+import java.util.*;
 
 public class ArraysCC {
     public static void main(String[] args) {
         int marks[] = new int[100];
+        Scanner sc = new Scanner(System.in);
+        marks[0] = sc.nextInt(); // phy
+        marks[1] = sc.nextInt(); // che
+        marks[2] = sc.nextInt(); // mat
         
     }
 }
