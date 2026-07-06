9236732e8a7b84f93a285a17459ee6c3066e443a
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-07 00:08:47 +0530

    auto update: 2026-07-07 00:08:47

--- arrays/LargestNumber.java
+++ arrays/LargestNumber.java
@@ -3,6 +3,13 @@ package arrays;
 public class LargestNumber {
     public static void main(String[] args){
         int numbers[] = {1,2,6,3,5};
-        int largest = 
+        int largest = Integer.MIN_VALUE;
+
+        for(int i = 0; i < numbers.length; i++){
+            if(numbers[i] > largest){
+                largest = numbers[i];
+            }
+        }
+        System.out.println(largest);
     }
 }
