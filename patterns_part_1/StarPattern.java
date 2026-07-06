589b92f58d271b03c3546c1e3d814c8b36536b81
Vedant Pawar <vedantpofficial@gmail.com>
2026-07-06 21:19:56 +0530

    auto update: 2026-07-06 21:19:56

--- patterns_part_1/StarPattern.java
+++ patterns_part_1/StarPattern.java
@@ -3,7 +3,7 @@ package patterns_part_1;
 public class StarPattern {
     public static void main(String[] args){
         for(int i = 0;i < 4; i++){
-            for(int j = 0; j < 4; j++){
+            for(int j = 0; j <= i; j++){
                 System.out.print("*");
             }
             System.out.println();
